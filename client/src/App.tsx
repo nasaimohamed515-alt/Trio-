1d8895141e2bd402ed0b189df87ea41b2506ed24
nasaimohamed515-alt <nasaimohamed515@gmail.com>
2026-02-03 22:15:11 -0600

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>TRIO Mentorship MVP</h1>
+      <p>Deployed successfully.</p>
+    </div>
+  );
+}
